b02582caba0da80af4d49725d296856a38764e48
vignesh1524 <43825542+vignesh1524@users.noreply.github.com>
2020-05-28 18:01:19 +0530

    Delete script.js

--- module4-solution/js/script.js
+++
@@ -1,9 +0,0 @@
-var a=['yaakov','Jen','paul','jasmine','reena','jameena','samuel'];
-for (var i=0 ; i < a.length; i++) {
-	var f = a[i].charAt(0).toLowerCase();
-	if (f == 'j') {
-		console.log("bye " + a[i]);
-	}else {
-		console.log("hello " + a[i]);
-	}
-}
